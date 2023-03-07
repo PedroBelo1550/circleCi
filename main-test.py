89d90e9f36a5f5730df72868fb1b031579dbea8c
pedro.belo <pedrohenrique1550@hotmail.com>
2023-03-07 19:25:19 -0300

    dfds

--- main-test.py
+++ main-test.py
@@ -2,9 +2,7 @@
 from main import Add
 
 def TestAdd():
-        assert Add(5,5) == 10 g i t add .
-g i t commit −m ‘ ‘ Adds t e s t t o make s u r e 5 + 5 i s e q u al t o 10”
-3 g i t push −−s e t−upstream o r i g i n add−t e s t
+        assert Add(5,5) == 10
         print("Add Function works correctly")
 
 if __name__ == '__main__':
